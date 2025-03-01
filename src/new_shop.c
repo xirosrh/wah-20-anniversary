80fb09cc3193115c27d506f328fe06237471bed5
mudskipper13 <MariaJumpMan001@hotmail.com>
2025-03-02 04:49:48 +0700

    New Shop: Move sellerId onto sMartInfo

--- src/new_shop.c
+++ src/new_shop.c
@@ -136,6 +136,7 @@ struct MartInfo
     u16 itemCount;
     u8 windowId;
     u8 martType;
+    u16 sellerId;
 };
 
 struct ShopData
@@ -147,7 +148,6 @@ struct ShopData
     u8 cursorSpriteId;
     u16 currentItemId;
     struct GridMenu *gridItems;
-    u16 sellerId;
 };
 
 struct Seller
@@ -620,7 +620,7 @@ void SetShopSellerId(void)
 
     if (gSpecialVar_LastTalked == 0) // failsafe
     {
-        sShopData->sellerId = SELLER_NONE;
+        sMartInfo.sellerId = SELLER_NONE;
         return;
     }
 
@@ -634,7 +634,7 @@ void SetShopSellerId(void)
     {
         if (gfxId == sSellers[i].id.gfxId)
         {
-            sShopData->sellerId = i;
+            sMartInfo.sellerId = i;
             break;
         }
     }
@@ -642,7 +642,7 @@ void SetShopSellerId(void)
 
 static inline const u8 *Shop_GetSellerMessage(enum Seller_MessageIds msgId)
 {
-    u32 sellerId = sShopData->sellerId;
+    u32 sellerId = sMartInfo.sellerId;
 
     if (sSellers[sellerId].message[msgId] == NULL)
     {
@@ -654,7 +654,7 @@ static inline const u8 *Shop_GetSellerMessage(enum Seller_MessageIds msgId)
 
 static const void *Shop_GetSellerGraphics(enum Seller_GraphicsIds gfxId)
 {
-    u32 sellerId = sShopData->sellerId;
+    u32 sellerId = sMartInfo.sellerId;
     const struct Seller *seller = &sSellers[sellerId];
 
     switch (gfxId)
@@ -1011,7 +1011,7 @@ static void BuyMenuInitBgs(void)
 #define DEFAULT_MENU_TILE_OFFSET 9
 static void BuyMenuDecompressBgGraphics(void)
 {
-    u32 i = sShopData->sellerId;
+    u32 i = sMartInfo.sellerId;
     if (gSpecialVar_LastTalked == 0 || i == SELLER_NONE)
     {
         DecompressAndCopyTileDataToVram(2, sNewShopMenu_DefaultMenuGfx, 0, DEFAULT_MENU_TILE_OFFSET, 0);
@@ -1202,7 +1202,7 @@ static void BuyMenuInitWindows(void)
 
 static bool32 LoadSellerCursor(void)
 {
-    u32 i = sShopData->sellerId;
+    u32 i = sMartInfo.sellerId;
     struct SpriteSheet gfx = {
         .data = Shop_GetSellerGraphics(SELLER_GFX_CURSOR_GFX),
         .size = 64*64*2,
