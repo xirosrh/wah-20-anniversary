2aec633e35658b76232798a0646d41cdff12a192
xirosrh <69046076+xirosrh@users.noreply.github.com>
2025-10-02 18:42:07 -0300

    set blax as sleeping

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -792,7 +792,7 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Blax = {
     .tracks = TRACKS_FOOT,
     .oam = &gObjectEventBaseOam_16x32,
     .subspriteTables = sOamTables_16x32,
-    .anims = sAnimTable_Standard,
+    .anims = sAnimTable_StandardWithSleeping,
     .images = sPicTable_Blax,
     .affineAnims = gDummySpriteAffineAnimTable,
 };
