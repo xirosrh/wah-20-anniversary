6665070798066dd8f20bba036255fc1972d17d05
xirosrh <69046076+xirosrh@users.noreply.github.com>
2025-10-03 23:20:56 -0300

    wip: jack johnson event

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -87,10 +87,10 @@
 #define FLAG_UNUSED_0x049    0x49 // Unused Flag
 #define FLAG_UNUSED_0x04A    0x4A // Unused Flag
 #define FLAG_UNUSED_0x04B    0x4B // Unused Flag
-#define FLAG_UNUSED_0x04C    0x4C // Unused Flag
-#define FLAG_UNUSED_0x04D    0x4D // Unused Flag
-#define FLAG_UNUSED_0x04E    0x4E // Unused Flag
-#define FLAG_UNUSED_0x04F    0x4F // Unused Flag
+#define FLAG_JACKROOM_HIDE_POKEBALL1    0x4C // Used WAH
+#define FLAG_JACKROOM_HIDE_POKEBALL2    0x4D // Used WAH
+#define FLAG_JACKROOM_HIDE_POKEBALL3    0x4E // Used WAH
+#define FLAG_JACKROOM_HIDE_POKEBALL4    0x4F // Used WAH
 
 // Scripts
 #define FLAG_HIDE_SKY_PILLAR_TOP_RAYQUAZA_STILL  0x50
