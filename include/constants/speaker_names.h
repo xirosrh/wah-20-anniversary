b6310ffcd153d14971022d5c474cf8b0d75f3321
xirosrh <69046076+xirosrh@users.noreply.github.com>
2025-12-06 15:47:03 -0300

    wip: fix jack script after map changes

--- include/constants/speaker_names.h
+++ include/constants/speaker_names.h
@@ -5,6 +5,7 @@ enum SpeakerNames {
     SP_NAME_NONE = 0,
     SP_NAME_MOM,
     SP_NAME_PLAYER,
+    SP_NAME_JACK_JOHNSON,
     SP_NAME_COUNT
 };
 
--- src/data/speaker_names.h
+++ src/data/speaker_names.h
@@ -2,4 +2,5 @@ const u8 *const gSpeakerNamesTable[SP_NAME_COUNT] =
 {
     [SP_NAME_MOM]    = COMPOUND_STRING("MOM"),
     [SP_NAME_PLAYER] = COMPOUND_STRING("{PLAYER}"),
+    [SP_NAME_JACK_JOHNSON]   = COMPOUND_STRING("JACK JOHNSON"),
 };
