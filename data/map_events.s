2b3c9c6004f5f402e830a4ffa51c640e059ede88
xirosrh <69046076+xirosrh@users.noreply.github.com>
2026-03-20 17:03:00 -0300

    refactor wah challenge clear flags logic

--- data/map_events.s
+++ data/map_events.s
@@ -14,6 +14,7 @@
 #include "constants/berry.h"
 #include "constants/species.h"
 #include "constants/apricorn_tree.h"
+#include "constants/var_values.h"
 	.include "asm/macros.inc"
 	.include "constants/constants.inc"
 
