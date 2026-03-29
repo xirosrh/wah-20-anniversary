6f4379ef0264132103cbdbac4436c454004fb081
xirosrh <69046076+xirosrh@users.noreply.github.com>
2026-03-29 02:01:43 -0300

    add dragon mountain map

--- src/data/tilesets/graphics.h
+++ src/data/tilesets/graphics.h
@@ -1994,3 +1994,41 @@ const u16 gTilesetPalettes_lobby[][16] =
 };
 
 const u32 gTilesetTiles_lobby[] = INCBIN_U32("data/tilesets/secondary/lobby/tiles.4bpp.lz");
+
+const u16 gTilesetPalettes_dragon_mountain[][16] =
+{
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/00.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/01.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/02.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/03.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/04.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/05.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/06.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/07.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/08.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/09.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/10.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/11.gbapal"),
+    INCBIN_U16("data/tilesets/primary/dragon_mountain/palettes/12.gbapal"),
+};
+
+const u32 gTilesetTiles_dragon_mountain[] = INCBIN_U32("data/tilesets/primary/dragon_mountain/tiles.4bpp.lz");
+
+const u16 gTilesetPalettes_dragon_mountain_2[][16] =
+{
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/00.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/01.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/02.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/03.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/04.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/05.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/06.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/07.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/08.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/09.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/10.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/11.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/dragon_mountain_2/palettes/12.gbapal"),
+};
+
+const u32 gTilesetTiles_dragon_mountain_2[] = INCBIN_U32("data/tilesets/secondary/dragon_mountain_2/tiles.4bpp.lz");
