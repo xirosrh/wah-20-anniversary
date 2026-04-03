781b3d34ae820b7e8e39d5a5e2e16f570b014e6a
xirosrh <69046076+xirosrh@users.noreply.github.com>
2026-04-02 22:03:50 -0300

    Añadir diálogo inicial del estadista si aún no ganó el desafío

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -236,10 +236,10 @@
 #define VAR_SOOTOPOLIS_WALLACE_STATE                     0x40D8
 #define VAR_HAS_TALKED_TO_SEAFLOOR_CAVERN_ENTRANCE_GRUNT 0x40D9
 #define VAR_REGISTER_BIRCH_STATE                         0x40DA
-#define VAR_UNUSED_0x40DB                                0x40DB // Unused Var
-#define VAR_UNUSED_0x40DC                                0x40DC // Unused Var
+#define VAR_WAH_CHALLENGE_STANDARD_COMPLETION_COUNT      0x40DB // WAH
+#define VAR_WAH_CHALLENGE_DOUBLE_COMPLETION_COUNT        0x40DC // WAH
 #define VAR_GIFT_PICHU_SLOT                              0x40DD
-#define VAR_GIFT_UNUSED_1                                0x40DE // Var is written to, but never read
+#define VAR_WAH_CHALLENGE_INVERSE_COMPLETION_COUNT       0x40DE // WAH
 #define VAR_GIFT_UNUSED_2                                0x40DF // Var is written to, but never read
 #define VAR_GIFT_UNUSED_3                                0x40E0 // Var is written to, but never read
 #define VAR_GIFT_UNUSED_4                                0x40E1 // Var is written to, but never read
