a8148461abee6e0614665377c81bc8cf023c4fdf
xirosrh <69046076+xirosrh@users.noreply.github.com>
2026-05-16 12:38:42 -0300

    add cope new challenge mode events

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -85,7 +85,7 @@
 #define FLAG_DEFEATED_COLLABORATORS_ROOM_RYUZAKI    0x47 // Used WAH
 #define FLAG_TRANSPARENT_BOX 0x48
 #define FLAG_WAH_CHALLENGE_INVERSE_COMPLETED    0x49 // Used WAH
-#define FLAG_EXPLAIN_NEW_CHALLENGE_MODES    0x4A // Used WAH
+#define FLAG_UNUSED_0x4A    0x4A // Unused - It was used in testing, so it is prefered to not be used anymore
 #define FLAG_LOBBY_POKEBOX_POST_WAH_UPDATE_SEEN    0x4B // First PC boot message after WAH Challenge (Pokebox software update)
 #define FLAG_WAH_CHALLENGE_STARTED_WITH_ELECTRODES        0x4C // Used WAH achievements
 #define FLAG_FOUND_TILE_KECLEON    0x4D // Used WAH
