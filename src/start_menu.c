e558274800efce599d2f9d27ecea95ec3ae267c7
xirosrh <69046076+xirosrh@users.noreply.github.com>
2026-05-21 09:09:00 -0300

    Use player gender color for save menu location

--- src/start_menu.c
+++ src/start_menu.c
@@ -1734,7 +1734,7 @@ static void ShowSaveInfoWindow(void)
 
     // Print region name
     yOffset = 1;
-    BufferSaveMenuText(SAVE_MENU_LOCATION, gStringVar4, TEXT_COLOR_GREEN);
+    BufferSaveMenuText(SAVE_MENU_LOCATION, gStringVar4, color);
     AddTextPrinterParameterized(sSaveInfoWindowId, FONT_NORMAL, gStringVar4, 0, yOffset, TEXT_SKIP_DRAW, NULL);
 
     // Print player name
